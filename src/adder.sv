6199b4e514f07a81ccc2d29df276fd61d57066b2
fstolzcode <16044340+fstolzcode@users.noreply.github.com>
2024-04-08 20:21:25 +0200

    new version, size reduced

--- src/adder.sv
+++ src/adder.sv
@@ -3,9 +3,9 @@
 // Company: 
 // Engineer: 
 // 
-// Create Date: 02/06/2024 07:50:50 PM
+// Create Date: 04/07/2024 06:21:56 PM
 // Design Name: 
-// Module Name: adder
+// Module Name: fpu_new
 // Project Name: 
 // Target Devices: 
 // Tool Versions: 
@@ -18,6 +18,7 @@
 // Additional Comments:
 // 
 //////////////////////////////////////////////////////////////////////////////////
+
 
 module fpu(
     input wire clk,
@@ -33,37 +34,61 @@ module fpu(
     output reg idle
     );
 
-localparam SIZE = 4           ;
-localparam ALU_IDLE  = 4'd0,
-ADD0 = 4'd1,ADD1 = 4'd2,ADD2 = 4'd3,ADD3 = 4'd4,ADD4 = 4'd5,
-SUB0 = 4'd6, SUB1=4'd7, SUB2=4'd8, SUB3 = 4'd9, SUB4=4'd10, SUB5=4'd11;
+
+parameter SIZE = 5;
+parameter ALU_IDLE  = 5'd0,
+ADD0 = 5'd1, ADD1 = 5'd2,ADD2 = 5'd3,ADD3 = 5'd4,ADD4 = 5'd5,
+ADD5 = 5'd6, ADD6= 5'd7, ADD7=5'd8, ADD8 = 5'd9, ADD9=5'd10, ADD10=5'd11, ADD11=5'd12, ADD12=5'd13, ADD13=5'd14, 
+SUB0=5'd15, SUB1= 5'd16, SUB2=5'd17, SUB3= 5'd18, SUB4=5'd19, SUB5= 5'd20, SUB6=5'd21, SUB7= 5'd22, SUB8=5'd23, SUB9= 5'd24,
+SUB10=5'd25;
 
 reg   [SIZE-1:0]          state        ;// Seq part of the FSM
 
-reg [6:0] af;
-reg [14:0] bf;
-reg [7:0] aa;
-reg [16:0] ba; // two front 
-reg [7:0] ab;
-reg [15:0] bb; // one front 
-wire signed [7:0] ae;
-wire [15:0] be; // one front
+reg[16:0] sa_in;
+reg[16:0] sb_in;
+reg sc_in;
+reg sadder_en;
+wire sadder_done;
+wire[16:0] sadder_out;
+wire sadder_cout;
 
-reg alu_a_c_in;
-reg alu_a_c_out;
-reg reg_alu_c_out;
-alu_a alu_a_inst(aa,ab,alu_a_c_in,ae,reg_alu_c_out);
+serial_adder sa(
+    .clk(clk),
+    .reset(reset),
+    .en(sadder_en),
+    .a(sa_in),
+    .b(sb_in),
+    .c_in(sc_in),
+    .done(sadder_done),
+    .out(sadder_out),
+    .c_out(sadder_cout)
+);
 
-reg [15:0] bb_shift_in;
-reg [4:0] bb_shift_amount;
-reg bb_left;
-reg [15:0] bb_shift_out;
-bb_shifter bb_shift_ins(bb_shift_in,bb_shift_amount,bb_left,bb_shift_out);
+reg sshifter_en;
+reg sshifter_left;
+wire sshifter_done;
+wire[16:0] sshifter_out;
+serial_shifter sh(
+    .clk(clk),
+    .reset(reset),
+    .en(sshifter_en),
+    .left(sshifter_left),
+    .a(sa_in),
+    .amount(sb_in[4:0]),
+    .done(sshifter_done),
+    .out(sshifter_out)
+);
 
-reg alu_b_c_in;
-reg alu_b_c_out;
-reg alu_b_cout;
-alu_b alu_b_inst(ba,bb,alu_b_c_in,be,alu_b_cout);
+reg[7:0] aa;
+reg[7:0] ab;
+reg[7:0] ae;
+
+reg[16:0] ba;
+reg[16:0] bb;
+reg[16:0] be;
+
+reg[3:0] temp;
+reg operation;
 
 always @ (posedge clk)
 begin : OUTPUT_LOGIC
@@ -72,183 +97,277 @@ begin : OUTPUT_LOGIC
   end else begin
       case(state)
       ALU_IDLE: begin
-          af <= 0;
-          bf <= 0;
-          aa <= 0;
-          ba <= 0;
-          ab <= 0;
-          bb <= 0;
-          alu_a_c_in <= 0;
-          alu_b_c_in <= 0;
-          bb_shift_in <= 0;
-          bb_shift_amount <= 0;
-          bb_left <= 0;
           idle <= 1;
+          
+          sadder_en <= 0;
+          sa_in <= 0;
+          sb_in <= 0;
+          sc_in <= 0;
+          
+          sshifter_en <= 0;
+          sshifter_left <= 0;
+          
+          aa <= 0;
+          ab <= 0;
+          ae <= 0;
+          
+          ba <= 0;
+          bb <= 0;
+          be <= 0;
+          
+          temp <= 0;
+          
           if(add == 1'b1) begin
             state <= ADD0;
+            operation <= 0;
           end else if (sub == 1'b1) begin
-            state <= SUB0;
+            state <= ADD0;
+            operation <= 1;
           end
       end
       ADD0 : begin
           idle <= 0;
-          aa <= {reg1_e[6],reg1_e[6:0]};
-          af <= reg1_e;     
-          ab <= {~reg2_e[6],~(reg2_e[6:0])};
-          bf <= reg1_m;
-          bb <= {1'b0,reg2_m};
-          alu_a_c_in <= 1;
+          sa_in <= { {10{reg1_e[6]}} , reg1_e};
+          sb_in <= { {10{~reg2_e[6]}} , ~reg2_e};
+          sc_in <= 1;
+          sadder_en <= 1;
           state <= ADD1;
           end
-        ADD1 : begin
-          if(ae >= 0) begin
-            ab <= 0;
-            aa <= {af[6],af[6:0]};
-            ba <= {2'b0,bf};
-            bb_shift_in <= be;
-            bb_left <= 0;
-            bb_shift_amount <= {8'b0,ae};
-          end else begin
-            aa <= 0;
-            ab <= ~ab;
-            ba <= {1'b0,be};
-            bb_shift_in <= {1'b0,bf};
-            bb_left <= 0;
-            bb_shift_amount <= {8'b0,-ae};
-          end
-           state <= ADD2;
-        end
-        ADD2 : begin
-            bb <= bb_shift_out;
+       ADD1 : begin
+         if(sadder_done == 1'b0) begin
+            state <= ADD2;
+         end
+         end
+       ADD2 : begin
+          sadder_en <= 0;
+          if(sadder_done == 1'b1) begin
+            ae <= sadder_out[7:0];
             state <= ADD3;
-        end
-        ADD3 : begin
-            if(be[15] == 1'b1) begin
-            bb_shift_in <= be;
-            bb_shift_amount <= 1;
-            bb_left <= 0;
-            alu_a_c_in <= 1;
-            end else begin
-            bb_shift_in <= be;
-            bb_shift_amount <= 0;
-            bb_left <= 0;
-            alu_a_c_in <= 0;
-            end
-             state <= ADD4;
-        end
-        ADD4: begin
-            res_e <= ae[6:0];
-            res_m <= bb_shift_out;
-            state <= ALU_IDLE;
-        end
-        // SUB
-        SUB0: begin
-          aa <= reg1_e;
-          af <= reg1_e;     
-          ab <= ~reg2_e;
-          bf <= reg1_m;
-          bb <= {1'b0,reg2_m};
-          alu_a_c_in <= 1;
-          state <= SUB1;
-        end
-        SUB1: begin
-            if(ae >= 0) begin
-            ab <= 0;
-            aa <= af;
-            ba <= {2'b0,bf};
-            bb_shift_in <= be;
-            bb_left <= 0;
-            bb_shift_amount <= {8'b0,ae};
-          end else begin
-            aa <= 0;
-            ab <= ~ab;
-            ba <= {1'b0,be};
-            bb_shift_in <= {1'b0,bf};
-            bb_left <= 0;
-            bb_shift_amount <= {8'b0,-ae};
           end
-          alu_a_c_in <= 0;
-          state <= SUB2;
-        end
-        SUB2: begin
-            bb <= ~bb_shift_out;
-            alu_b_c_in <= 1;
-            state <= SUB3;
-        end
-        SUB3: begin
-           aa <= ae;
-           ab <= 0;
-           ba <= 0;
-           if (be[15] == 1'b0) begin
-              bb <= be;
-              alu_b_c_in <= 0;
-           end else begin
-                //assert(1 == 0);
-                bb <= ~be;
-                alu_b_c_in <= 1;
-           end
-           state <= SUB4;
-        end
-        SUB4: begin
-           aa <= ae;
-           bb_shift_in <= be;
-           bb_left <= 1;
-           if(be[14] == 1'b1) begin
-           ab <= ~8'b0;
-           bb_shift_amount <= 0;
-           end else begin
-                if(be[13] == 1'b1) begin
-                ab <= ~8'd1;
-                bb_shift_amount <= 5'd1;
-                end else if(be[12] == 1'b1) begin
-                ab <= ~8'd2;
-                bb_shift_amount <= 5'd2;
-                end else if(be[11] == 1'b1) begin
-                ab <= ~8'd3;
-                bb_shift_amount <= 5'd3;
-                end else if(be[10] == 1'b1) begin
-                ab <= ~8'd4;
-                bb_shift_amount <= 5'd4;
-                end else if(be[9] == 1'b1) begin
-                ab <= ~8'd5;
-                bb_shift_amount <= 5'd5;
-                end else if(be[8] == 1'b1) begin
-                ab <= ~8'd6;
-                bb_shift_amount <= 5'd6;
-                end else if(be[7] == 1'b1) begin
-                ab <= ~8'd7;
-                bb_shift_amount <= 5'd7;
-                end else if(be[6] == 1'b1) begin
-                ab <= ~8'd8;
-                bb_shift_amount <= 5'd8;
-                end else if(be[5] == 1'b1) begin
-                ab <= ~8'd9;
-                bb_shift_amount <= 5'd9;
-                end else if(be[4] == 1'b1) begin
-                ab <= ~8'd10;
-                bb_shift_amount <= 5'd10;
-                end else if(be[3] == 1'b1) begin
-                ab <= ~8'd11;
-                bb_shift_amount <= 5'd11;
-                end else if(be[2] == 1'b1) begin
-                ab <= ~8'd12;
-                bb_shift_amount <= 5'd12;
-                end else if(be[1] == 1'b1) begin
-                ab <= ~8'd13;
-                bb_shift_amount <= 5'd13;
-                end else if(be[0] == 1'b1) begin
-                ab <= ~8'd14;
-                bb_shift_amount <= 5'd14;
+          end
+       ADD3: begin
+            if(ae[7] == 1'b0) begin
+                ab <= 0;
+                aa <= {reg1_e[6] ,reg1_e};
+                
+                ba <= {2'b0,reg1_m};
+                sa_in <= {2'b0,reg2_m};
+                sb_in <= {9'b0, ae};
+                sshifter_left <= 0;
+                sshifter_en <= 1;
+                state <= ADD4;
+            end else begin
+                aa <= 0;
+                ab <= {reg2_e[6] ,reg2_e};
+                
+                ba <= {2'b0,reg2_m};
+                sa_in <= { {9{~ae[7]}}, ~ae};
+                sb_in <= 17'b1;
+                sc_in <= 1'b0;
+                sadder_en <= 1;
+                state <= ADD12;
+            end
+       end
+       ADD4 : begin
+         if(sshifter_done == 1'b0) begin
+            state <= ADD5;
+         end
+         end
+       ADD5 : begin
+          sshifter_en <= 0;
+          if(sshifter_done == 1'b1 && operation == 1'b0) begin
+            bb <= sshifter_out;
+            state <= ADD6;
+          end else if(sshifter_done == 1'b1 && operation == 1'b1) begin
+            bb <= sshifter_out;
+            state <= SUB0;
+          end
+          end
+       ADD6: begin
+          sa_in <= ba;
+          sb_in <= bb;
+          sc_in <= 0;
+          sadder_en <= 1;
+          state <= ADD7;
+       end
+       ADD7 : begin
+         if(sadder_done == 1'b0) begin
+            state <= ADD8;
+         end
+         end
+        ADD8 : begin
+          sadder_en <= 0;
+          if(sadder_done == 1'b1) begin
+            be <= sadder_out;
+            state <= ADD9;
+          end
+          end
+         ADD9: begin
+            if( (be[16] | be[15]) == 1'b1) begin
+               sa_in <= {9'b0, aa};
+               sb_in <= {9'b0, ab};
+               sc_in <= 1;
+               sadder_en <= 1;
+               state <= ADD10;
+            end else begin
+               res_e <= aa[6:0] | ab[6:0];
+               res_m <= be[14:0];
+               state <= ALU_IDLE;
+            end
+         end
+         ADD10: begin
+            if(sadder_done == 1'b0) begin
+            state <= ADD11;
+            end
+         end
+         ADD11: begin
+            sadder_en <= 0;
+            if(sadder_done == 1'b1) begin
+             res_e <= sadder_out[6:0];
+             res_m <= be[15:1];
+             state <= ALU_IDLE;
+          end
+         end
+         ADD12: begin
+            if(sadder_done == 1'b0) begin
+            state <= ADD13;
+            end
+         end
+         ADD13: begin
+            sadder_en <= 0;
+            if(sadder_done == 1'b1) begin
+                sa_in <= {2'b0,reg1_m};
+                sb_in <= sadder_out;
+                sshifter_left <= 0;
+                sshifter_en <= 1;
+                state <= ADD4;
+            end
+         end
+         ////////////// SUB
+         SUB0: begin
+            sa_in <= ba;
+            sb_in <= ~bb;
+            sc_in <= 1;
+            sadder_en <= 1;
+            state <= SUB1;
+         end
+         SUB1: begin
+            if(sadder_done == 1'b0) begin
+            state <= SUB2;
+            end
+         end
+         SUB2: begin
+            sadder_en <= 0;
+            if(sadder_done == 1'b1 && sadder_out[16] == 1'b1) begin
+                //NEGATIVE
+                sa_in <= ~sadder_out;
+                sb_in <= 17'b1;
+                sc_in <= 0;
+                sadder_en <= 1;
+                state <= SUB3;
+            end else if(sadder_done == 1'b1 && sadder_out[16] == 1'b0) begin
+                state <= SUB4;
+            end
+         end
+         SUB3: begin
+            if(sadder_done == 1'b0) begin
+            state <= SUB4;
+            end
+         end
+         SUB4: begin
+            sadder_en <= 0;
+            if(sadder_done == 1'b1) begin
+                be <= sadder_out;
+                //
+                if(sadder_out[14] == 1'b1) begin
+                temp <= 4'd0;
+                end else if(sadder_out[13] == 1'b1) begin
+                temp <= 4'd1;
+                end else if(sadder_out[12] == 1'b1) begin
+                temp <= 4'd2;
+                end else if(sadder_out[11] == 1'b1) begin
+                temp <= 4'd3;
+                end else if(sadder_out[10] == 1'b1) begin
+                temp <= 4'd4;
+                end else if(sadder_out[9] == 1'b1) begin
+                temp <= 4'd5;
+                end else if(sadder_out[8] == 1'b1) begin
+                temp <= 4'd6;
+                end else if(sadder_out[7] == 1'b1) begin
+                temp <= 4'd7;
+                end else if(sadder_out[6] == 1'b1) begin
+                temp <= 4'd8;
+                end else if(sadder_out[5] == 1'b1) begin
+                temp <= 4'd9;
+                end else if(sadder_out[4] == 1'b1) begin
+                temp <= 4'd10;
+                end else if(sadder_out[3] == 1'b1) begin
+                temp <= 4'd11;
+                end else if(sadder_out[2] == 1'b1) begin
+                temp <= 4'd12;
+                end else if(sadder_out[1] == 1'b1) begin
+                temp <= 4'd13;
+                end else if(sadder_out[0] == 1'b1) begin
+                temp <= 4'd14;
                 end
-           end
-            alu_a_c_in <= 1;
-            state <= SUB5;
-        end
-        SUB5: begin
-        res_e <= ae;
-        res_m <= bb_shift_out[14:0];
-        state <= ALU_IDLE;
-        end
+                //
+                sa_in <= {9'b0, aa};
+                sb_in <= {9'b0, ab};
+                sc_in <= 0;
+                sadder_en <= 1;
+                state <= SUB5;
+            end
+         end
+         SUB5: begin
+            if(sadder_done == 1'b0) begin
+            state <= SUB6;
+            end
+         end
+         SUB6: begin
+            sadder_en <= 0;
+            if(sadder_done == 1'b1) begin
+                if(temp == 4'd0) begin
+                    res_e <= sadder_out[6:0];
+                    res_m <= be[14:0];
+                    state <= ALU_IDLE;
+                end else begin
+                    sa_in <= sadder_out;
+                    sb_in <= {12'b1,~temp};
+                    sc_in <= 1;
+                    sadder_en <= 1;
+                    state <= SUB7;
+                end
+            end
+         end
+         SUB7: begin
+            if(sadder_done == 1'b0) begin
+                state <= SUB8;
+            end
+         end
+         SUB8: begin
+            sadder_en <= 0;
+            if(sadder_done == 1'b1) begin
+                res_e <= sadder_out[6:0];
+                sa_in <= be;
+                sb_in <= {12'b0,temp};
+                sc_in <= 0;
+                sshifter_left <= 1;
+                sshifter_en <= 1;
+                state <= SUB9;
+            end
+         end
+         SUB9: begin
+            if(sadder_done == 1'b0) begin
+                state <= SUB10;
+            end
+         end
+         SUB10: begin
+            sadder_en <= 0;
+            if(sadder_done == 1'b1) begin
+                res_m <= sadder_out[14:0];
+                state <= ALU_IDLE;
+            end
+         end
       default: begin
         state <= ALU_IDLE;
       end
--- src/alu.sv
+++ src/alu.sv
@@ -1,111 +1,226 @@
-`timescale 1ns / 1ps
-//////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date: 02/07/2024 05:11:17 PM
-// Design Name: 
-// Module Name: alu
-// Project Name: 
-// Target Devices: 
-// Tool Versions: 
-// Description: 
-// 
-// Dependencies: 
-// 
-// Revision:
-// Revision 0.01 - File Created
-// Additional Comments:
-// 
-//////////////////////////////////////////////////////////////////////////////////
-
-
-// Code your design here
-module alu_a (
-    input [7:0] a,
-    input [7:0] b,
-    input c_in,
-    output [7:0] out,
-    output c_out
+module full_adder(
+  input wire a,
+  input wire b,
+  input wire c_in,
+  output wire out,
+  output wire c_out
 );
-    wire [7:0] c;
-    assign out[0] = a[0] ^ b[0] ^ c_in;
-  assign c[0] = (a[0] & b[0]) | (a[0] & c_in) | (b[0] & c_in);
-    genvar i;
-    generate for(i = 1; i < 8; i = i + 1) begin
-      assign out[i] = a[i] ^ b[i] ^ c[i-1];
-      assign c[i] = (a[i] & b[i]) | (a[i] & c[i-1]) | (b[i] & c[i-1]);
+  assign out = a ^ b ^ c_in;
+  assign c_out =  (a & b) | (a & c_in) | (b & c_in);
+endmodule
+
+module shift_register(
+  input wire clk,
+  input wire reset,
+  input wire en,
+  input wire latch,
+  input wire left,
+  input wire shift_in,
+  input wire [16:0] a,
+  output wire [16:0] out
+);
+  reg [16:0] shift_reg;
+  assign out = shift_reg;
+  always @(posedge clk) begin
+    if(reset == 1'b1) begin
+      shift_reg <= 0;
+    end else if(latch == 1'b1) begin
+      shift_reg <= a;
+    end else if(en == 1'b1) begin
+      if(left == 1'b1) begin
+        shift_reg <= {shift_reg[15:0],shift_in};
+      end else begin
+        shift_reg <= {shift_in,shift_reg[16:1]};
+      end
     end
-    assign c_out = c[7];
-    endgenerate
+  end
 endmodule
 
-module ba_shifter (
-    input [14:0] in,
-    input [1:0] amount,
-    input left,
-    output [16:0] out
+module serial_shifter(
+    input wire clk,
+    input wire reset,
+    input wire en,
+    input wire left,
+    input wire [16:0] a,
+    input wire [4:0] amount,
+    output reg done,
+    output reg [16:0] out
+    
 );
-
-reg [14:0] out_reg;
-always_comb begin 
-    case (amount)
-      2'd1: out_reg = left == 1'b1 ? {1'b0, in[13:0], 1'b0} : {3'b0,in[14:1]};
-      2'd2: out_reg = left == 1'b1 ? {2'b0,in[12:0], 1'b0} : {4'b0,in[14:2]};
-    default: out_reg = in;
-    endcase
-end
-assign out = out_reg;
-endmodule
-
-module bb_shifter (
-    input [15:0] in,
-    input [4:0] amount,
-    input left,
-    output [15:0] out
-);
-
-reg [15:0] out_reg;
-always_comb begin 
-    case (amount)
-      5'd1: out_reg = left == 1'b1 ? {in[14:0],1'b0} : {1'b0,in[15:1]};
-      5'd2: out_reg = left == 1'b1 ? {in[13:0],2'b0} : {2'b0,in[15:2]};
-      5'd3: out_reg = left == 1'b1 ? {in[12:0],3'b0} : {3'b0,in[15:3]};
-      5'd4: out_reg = left == 1'b1 ? {in[11:0],4'b0} : {4'b0,in[15:4]};
-      5'd5: out_reg = left == 1'b1 ? {in[10:0],5'b0} : {5'b0,in[15:5]};
-      5'd6: out_reg = left == 1'b1 ? {in[9:0],6'b0} : {6'b0,in[15:6]};
-      5'd7: out_reg = left == 1'b1 ? {in[8:0],7'b0} : {7'b0,in[15:7]};
-      5'd8: out_reg = left == 1'b1 ? {in[7:0],8'b0} : {8'b0,in[15:8]};
-      5'd9: out_reg = left == 1'b1 ? {in[6:0],9'b0} : {9'b0,in[15:9]};
-      5'd10: out_reg = left == 1'b1 ? {in[5:0],10'b0} : {10'b0,in[15:10]};
-      5'd11: out_reg = left == 1'b1 ? {in[4:0],11'b0} : {11'b0,in[15:11]};
-      5'd12: out_reg = left == 1'b1 ? {in[3:0],12'b0} : {12'b0,in[15:12]};
-      5'd13: out_reg = left == 1'b1 ? {in[2:0],13'b0} : {13'b0,in[15:13]};
-      5'd14: out_reg = left == 1'b1 ? {in[1:0],14'b0} : {14'b0,in[15:14]};
-      5'd15: out_reg = left == 1'b1 ? {in[0],15'b0} : {15'b0,in[15]};
-      5'd16: out_reg = left == 1'b1 ? {16'b0} : {16'b0};
-    default: out_reg = in;
-    endcase
-end
-assign out = out_reg;
-endmodule
-
-module alu_b (
-    input [16:0] a,
-    input [15:0] b,
-    input c_in,
-    output [15:0] out,
-    output c_out
-);
-    wire [16:0] b_ex = {1'b0,b[15:0]};
-    wire [16:0] c;
-    assign out[0] = a[0] ^ b_ex[0] ^ c_in;
-    assign c[0] = (a[0] & b_ex[0]) | (a[0] & c_in) | (b_ex[0] & c_in);
-    genvar i;
-    generate for(i = 1; i < 16; i = i + 1) begin
-      assign out[i] = a[i] ^ b_ex[i] ^ c[i-1];
-      assign c[i] = (a[i] & b_ex[i]) | (a[i] & c[i-1]) | (b_ex[i] & c[i-1]);
+   reg shift_en;
+   reg shift_latch;
+   wire [16:0] shift_out;
+   shift_register internal_shift(
+    .clk(clk),
+    .reset(reset),
+    .en(shift_en),
+    .latch(shift_latch),
+    .left(left),
+    .shift_in(1'b0),
+    .a(a),
+    .out(shift_out)
+  );
+  
+  parameter FSMS_SIZE = 3;
+  parameter FSMS_IDLE = 3'd0, FSMS_LATCH = 3'd1, FSMS_LATCH_SETTLE = 3'd2, FSMS_SHIFT = 3'd3, FSMS_RESULT = 3'd4;
+  reg [FSMS_SIZE-1:0] state;
+  reg [4:0] cnt;
+  always @(posedge clk) begin
+    if(reset == 1'b1) begin
+        out <= 0;
+        done <= 0;
+        shift_en <= 0;
+        shift_latch <= 0;
+        cnt <= 0;
+        state <= FSMS_IDLE;
+    end else begin
+        case(state)
+            FSMS_IDLE: begin
+                done <= 1;
+                shift_en <= 0;
+                shift_latch <= 0;
+                cnt <= 0;
+                if(en == 1'b1) begin
+                    state <= FSMS_LATCH;
+                end
+            end
+            FSMS_LATCH: begin
+                done <= 0;
+                shift_latch <= 1;
+                state <= FSMS_LATCH_SETTLE;
+            end
+            FSMS_LATCH_SETTLE: begin
+                shift_latch <= 0;
+                state <= FSMS_SHIFT;
+            end
+            FSMS_SHIFT: begin
+                if(cnt == amount) begin
+                    shift_en <= 0;
+                    state <= FSMS_RESULT;
+                end else begin
+                     shift_en <= 1;
+                     cnt <= cnt + 1'b1;
+                end
+            end
+            FSMS_RESULT: begin
+                out <= shift_out;
+                state <= FSMS_IDLE;
+            end
+        endcase
     end
-      assign c_out = (a[16] & b_ex[16]) | (a[16] & c[15]) | (b_ex[16] & c[15]);
-    endgenerate
+  end
+endmodule
+
+module serial_adder(
+  input wire clk,
+  input wire reset,
+  input wire en,
+  input wire [16:0] a,
+  input wire [16:0] b,
+  input c_in,
+  output reg done,
+  output reg [16:0] out,
+  output reg c_out
+);
+  
+  reg shift_en;
+  reg shift_latch;
+  wire [16:0] shift_out_a;
+  shift_register a_shift(
+    .clk(clk),
+    .reset(reset),
+    .en(shift_en),
+    .latch(shift_latch),
+    .left(1'b0),
+    .shift_in(1'b0),
+    .a(a),
+    .out(shift_out_a)
+  );
+  
+  wire [16:0] shift_out_b;
+  shift_register b_shift(
+    .clk(clk),
+    .reset(reset),
+    .en(shift_en),
+    .latch(shift_latch),
+    .left(1'b0),
+    .shift_in(1'b0),
+    .a(b),
+    .out(shift_out_b)
+  );
+  
+  wire [16:0] shift_out_result;
+  wire fa_out;
+  shift_register result_shift(
+    .clk(clk),
+    .reset(reset),
+    .en(shift_en),
+    .latch(shift_latch),
+    .left(1'b0),
+    .shift_in(fa_out),
+    .a(17'b0),
+    .out(shift_out_result)
+  );
+  
+  wire fa_c_out;
+  reg fa_c_in;
+  full_adder full_adder_ins(
+    .a(shift_out_a[0]),
+    .b(shift_out_b[0]),
+    .c_in(fa_c_in),
+    .out(fa_out),
+    .c_out(fa_c_out)
+  );
+  
+  parameter FSMA_SIZE = 3;
+  parameter FSMA_IDLE = 3'd0, FSMA_LATCH = 3'd1, FSMA_LATCH_SETTLE = 3'd2, FSMA_ADD = 3'd3, FSMA_RESULT = 3'd4;
+  reg [FSMA_SIZE-1:0] state;
+  reg [4:0] cnt;
+  always @(posedge clk) begin
+    if (reset == 1'b1) begin
+        state <= FSMA_IDLE;
+      	cnt <= 0;
+      	done <= 0;
+     	shift_en <= 0;
+      	shift_latch <= 0;
+      	fa_c_in <= 0;
+      	c_out <= 0;
+      	out <= 0;
+    end else begin
+      case(state)
+        FSMA_IDLE: begin
+          cnt <= 0;
+      	  done <= 1;
+     	  shift_en <= 0;
+      	  shift_latch <= 0;
+      	  fa_c_in <= 0;
+          if(en == 1'b1) begin
+            state <= FSMA_LATCH;
+          end
+        end
+        FSMA_LATCH: begin
+          done <= 0;
+          shift_latch <= 1;
+          state <= FSMA_LATCH_SETTLE;
+        end
+        FSMA_LATCH_SETTLE: begin
+          shift_latch <= 0;
+          state <= FSMA_ADD;
+        end
+        FSMA_ADD: begin
+          shift_en <= 1;
+		  fa_c_in <= (cnt == 5'd0) ? c_in : fa_c_out;
+          cnt <= cnt + 5'd1;
+          if(cnt == 5'd17) begin
+            c_out <= fa_c_out;
+            state <= FSMA_RESULT;
+          end
+        end
+        FSMA_RESULT: begin
+            out <= shift_out_result;
+            state <= FSMA_IDLE;
+        end
+      endcase
+    end
+  end
 endmodule
